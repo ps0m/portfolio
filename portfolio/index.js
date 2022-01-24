9b24b61c06188ec6a0b353cc66a5e3a0bd885f2a
ps0m <ps0m@tut.by>
2022-01-24 23:35:13 +0300

    feat: add close after click nav-link

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,23 +1,36 @@
-console.log(`
-Общий балл: 81
-Вёрстка соответствует макету. Ширина экрана 768px +48
-Ни на одном из разрешений до 320px включительно
-не появляется горизонтальная полоса прокрутки.
-Весь контент страницы при этом сохраняется:
-не обрезается и не удаляется +15
-На ширине экрана 768рх и меньше реализовано адаптивное меню +18
-`);
+// console.log(`
+// Общий балл: 81
+// Вёрстка соответствует макету. Ширина экрана 768px +48
+// Ни на одном из разрешений до 320px включительно
+// не появляется горизонтальная полоса прокрутки.
+// Весь контент страницы при этом сохраняется:
+// не обрезается и не удаляется +15
+// На ширине экрана 768рх и меньше реализовано адаптивное меню +18
+// `);
 
 //Burger function
 
 (function() {
-    const burgerItem = document.querySelector('.burger');
+    
+    const clickBurger = document.querySelector('.burger');
     const menu = document.querySelector('.header_nav');
-    const menuClose = document.querySelector ('.nav_close');
-    burgerItem.addEventListener('click', ()=> {
+    const clickCross = document.querySelector ('.nav_close');
+    const navLinks = document.querySelectorAll ('.header_link');
+    
+    clickBurger.addEventListener('click', ()=> {
         menu.classList.toggle('header_nav_active');
     }); 
-    menuClose.addEventListener ('click', ()=> {
+
+    clickCross.addEventListener ('click', ()=> {
         menu.classList.toggle('header_nav_active');
     });
+
+    function closeMenu (event) {
+        if (!event.target.classList.contains ('header_nav_active')){
+            menu.classList.remove('header_nav_active');
+        };
+    }
+
+    navLinks.forEach((el) => el.addEventListener('click', closeMenu ));
+   
 }());
